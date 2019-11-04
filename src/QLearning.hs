a2f000b592365f7e0ea816b84ecbc8b672b91457
am.ts <am.ts@eemutsnoMacBook-Pro.local>
2019-11-05 00:29:12 +0900

    add: more logic

--- src/QLearning.hs
+++ src/QLearning.hs
@@ -1,43 +1,55 @@
 module Env where
 
 import qualified Data.Map                      as M
+import System.Random
+import Data.Random
+
 main :: IO ()
 main = do
-    let qTable = initQMap 2 2
+    let states = State
+    let qTable = initQMap State
     putStrLn "1"
 
 
 data Action = UP | DOWN | LEFT | RIGHT deriving (Show)
 
-type QMap = M.Map
+type QMap = M.Map String Int
 type State = (Int, Int)
 type Height = Int
 type Width = Int
 type Field = (Height, Width)
 
+
 initQMap :: Int -> Int -> QMap
 initQMap _ _ = M
 
-qFunc :: State -> Action -> QMap -> Int
-Func _ _ _ = 1
+qFunc :: State -> QMap -> Action -> Int
+qFunc _ _ _ = 1
 
 reward :: State -> Action -> Int
 reward _ _ = 1
 
-legalAction :: State -> Field -> Action
-legalAction _ _ = UP
 
+legalAction :: State -> Field -> [Action]
+legalAction _ _ = [UP]
+
+maxAction ::  QMap -> [Action] -> Action
+maxAction qmap actions = maxfa (qFunc qmap) actions
+
+epsilonGreedy :: Double -> Action -> [Action] -> Action 
+epsilonGreedy epsilon maxAction actions 
+    | adventure = randomElement actions
+    | otherwise = maxAction
+    where adventure =  (epsilon >) <$> (randomRIO (0, 1) :: IO Double)
 
 updateQFunc :: Int -> QMap -> QMap
 updateQFunc _ _ = M
 
+maxfa :: (Ord b) => (a -> b) -> [a] -> a
+maxfa f (a:as)
+    | f a > maxfb as  = a      
+    | otherwise = maxfa as
+maxfa f a = a
 
-
-
-
-
-
-
-
-
-
+maxfb :: (Ord b) => (a -> b) -> [a] -> b
+maxfb f as = maximum $ map f as 
