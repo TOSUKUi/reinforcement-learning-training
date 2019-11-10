dc64bb204e55eb2ec50e941dc9ecbf7776ca3883
TOSUKUi <am.businessmail.exp@gmail.com>
2019-11-11 02:16:42 +0900

    add: any of part ,

--- src/QLearning.hs
+++ src/QLearning.hs
@@ -6,7 +6,8 @@ import           Data.Random
 
 main :: IO ()
 main = do
-    let states = State
+
+    let states = ()
     let qTable = initQMap State
     putStrLn "1"
 
@@ -15,10 +16,21 @@ data Action = UP | DOWN | LEFT | RIGHT deriving (Show, Ord, Eq)
 
 type QMap = M.Map QIndex Int
 type State = (Int, Int)
+type CakePosition = (Int, Int)
 type QIndex = (State, Action)
 type Height = Int
 type Width = Int
 type Field = (Height, Width)
+
+
+step :: [Action] -> State -> QMap -> Double -> State
+step actions state qmap random = do
+    let legalActions = legalAction actions
+    let randomChoice <- randomRIO 0 $ length legalAction
+    let adventure = (1 - epsilon
+
+
+
 
 
 initQMap :: [Action] -> [State] -> QMap
@@ -28,12 +40,42 @@ initQMap actions states =
 qFunc :: State -> QMap -> Action -> Maybe Int
 qFunc state qmap action = M.lookup (state, action) qmap
 
-reward :: State -> Action -> Int
-reward _ = 1
+reward :: State -> Action -> CakePosition -> Int
+
+reward state action cakePosition | nextCakeDistance == 0            = 10
+                                 | cakeDistance > nextCakeDistance  = -1
+                                 | cakeDistance == nextCakeDistance = 0
+                                 | cakeDistance < nextCakeDistance  = 1
+  where
+    cakeDistance     = manlen state cakePosition
+    nextCakeDistance = manlen (nextState state action) cakePosition
 
 
-legalAction :: State -> Field -> [Action]
-legalAction _ _ = [UP]
+
+manlen :: (Int, Int) -> (Int, Int) -> Int
+manlen p1 p2 = (abs $ (fst p1) - (fst p2)) + (abs $ (snd p1) - (snd p2))
+
+
+
+nextState :: State -> Action -> State
+nextState state action | action == UP    = (fst state, (snd state) + 1)
+                       | action == DOWN  = (fst state, (snd state) - 1)
+                       | action == RIGHT = ((fst state) + 1, snd state)
+                       | action == LEFT  = ((fst state) - 1, snd state)
+
+
+legalAction :: State -> [Action] -> Field -> Field -> [Action]
+legalAction state actions fieldmin fieldmax =
+    filter ((legalState fieldmin fieldmax) . (nextState state)) actions
+
+legalState :: Field -> Field -> State -> Bool
+legalState state fieldmin fieldmax = and
+    [ (fst fieldmin) < (fst state)
+    , (fst state) < (fst fieldmax)
+    , (snd fieldmin) < (snd state)
+    , (snd state) < (snd fieldmax)
+    ]
+
 
 maxAction :: State -> QMap -> [Action] -> Action
 maxAction state qmap actions = maxfa (qFunc state qmap) actions
@@ -43,8 +85,10 @@ epsilonGreedy randomChoice adventure maxAction actions
     | adventure = actions !! randomChoice
     | otherwise = maxAction
 
-updateQFunc :: Int -> QMap -> QMap
-updateQFunc _ _ = M
+updateQFunc :: Int -> QIndex -> QMap -> QMap
+updateQFunc updateAmount qindex qmap =
+    M.update (Just . (+ updateAmount)) qindex qmap
+
 
 maxfa :: (Ord b) => (a -> b) -> [a] -> a
 maxfa f (a : as) | f a > (maxfb f as) = a
